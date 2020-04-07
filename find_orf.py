430b7708869d259da91c4558537d622ae4254bf9
Shelby C. Osburn <sco0004@auburn.edu>
2020-04-06 19:58:07 -0500

    fixing reg ex for recognizing RNA and DNA pattern

--- find_orf.py
+++ find_orf.py
@@ -57,8 +57,8 @@ def vet_nucleotide_sequence(sequence):
     # any valid RNA and DNA sequence strings, respectively (and only strings of
     # RNA and DNA bases).
     # Read the docstring above for additional clues.
-    rna_pattern_str = r'AUCG'
-    dna_pattern_str = r'ATCG'
+    rna_pattern_str = r'(?!.*T)[AUCG]*'
+    dna_pattern_str = r'(?!.*U)[ATCG]*'
     ##########################################################################
 
     rna_pattern = re.compile(rna_pattern_str)
